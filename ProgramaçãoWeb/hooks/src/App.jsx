59669992a5b5e51b5a9cbbe37d268afcd1dfc904
VictorHugo016 <victor.braga016@gmail.com>
2025-04-11 08:29:58 -0300

    pw

--- ProgramaçãoWeb/hooks/src/App.jsx
+++ ProgramaçãoWeb/hooks/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+import Exercicio01 from './exercicios/01'
+import Exercicio02 from './exercicios/02'
+import Exercicio03 from './exercicios/03'
+import Exercicio04 from './exercicios/04'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Hooks</h1>
+
+      <BrowserRouter>
+        <ul>
+          <li> <Link to="/">Início</Link> </li>
+          <li> <Link to="/01">Exercício 01</Link> </li>
+          <li> <Link to="/02">Exercício 02</Link> </li>
+          <li> <Link to="/03">Exercício 03</Link> </li>
+          <li> <Link to="/04">Exercício 04</Link> </li>
+        </ul>
+        
+        <hr />
+
+        <Routes>
+          <Route path="/" element={
+            <p>Clique em um dos <em>links</em> acima para exibir um exercício.</p>
+          } />
+
+          <Route path="/01" element={ <Exercicio01 /> } />
+          <Route path="/02" element={ <Exercicio02 /> } />
+          <Route path="/03" element={ <Exercicio03 /> } />
+          <Route path="/04" element={ <Exercicio04 /> } />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
